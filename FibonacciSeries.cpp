48ee62b2be77bf09af75cceaf7e91297d7123645
Saiyam Tuteja <119167105+SaiyamTuteja@users.noreply.github.com>
2025-02-16 00:17:36 +0530

    Add files via upload

---
+++ FibonacciSeries.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Fibonacci Series in Cpp: In case of fibonacci series, next number is the sum of previous two numbers for
+//  example 0, 1, 1, 2, 3, 5, 8, 13, 21 etc. The first two numbers of fibonacci series are 0 and 1.
+int main()
+{
+    int n = 0;
+    int n1 = 1;
+    int n2;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << n << endl;
+        n2 = n + n1;
+        n = n1;
+        n1 = n2;
+    }
+    return 0;
+}
