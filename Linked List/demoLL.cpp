077849fbe88d36f1ae4790d24379526874a682e3
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-03-11 23:01:13 +0530

    update

--- Linked List/demoLL.cpp
+++ Linked List/demoLL.cpp
@@ -108,4 +108,20 @@ public:
 };
 int main()
 {
+    List l;
+    l.push_back(1);
+    l.push_back(2);
+    l.push_back(3);
+    l.push_back(4);
+    l.push_back(5);
+    l.print_list();
+    l.Serach(3);
+    l.Serach(6);
+    l.pop_front();
+    l.print_list();
+    l.pop_back();
+    l.print_list();
+    l.push_front(0);
+    l.print_list();
+    return 0;
 }
