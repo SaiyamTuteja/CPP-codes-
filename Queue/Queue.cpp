31f56bc1d068cfe0df3bb7b21f1992c025c7d8a6
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-03-14 01:11:06 +0530

    update

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -1,5 +1,27 @@
 #include<bits/stdc++.h>
 #include<queue>
+int front=0;
+int rear=0;
+int size=0;
+int arr[100];
+void push(int x){
+    if(size==100){
+        cout<<"Queue is full"<<endl;
+        return;
+    }
+    arr[rear]=x;
+    rear++;
+    size++;
+}
+void pop(){
+    if(size==0){
+        cout<<"Queue is empty"<<endl;
+        return;
+    }
+    front++;
+    size--;
+}
+
 using namespace std;
 int main(){
     
