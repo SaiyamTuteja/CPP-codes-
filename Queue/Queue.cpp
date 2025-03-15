0b446be4eab3adc4b1d80fed9562abdc4c3a03cb
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-03-16 00:25:02 +0530

    update

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -76,3 +76,16 @@ int main()
 
     return 0;
 }
+
+//  queueSTL.cpp
+// #include <bits/stdc++.h>
+// #include <queue>
+// using namespace std;
+//
+// int main()
+// {
+//     queue<int> q;
+//     q.push(1);
+//     q.push(2);
+//     q.push(3);
+//     q.push(4);
