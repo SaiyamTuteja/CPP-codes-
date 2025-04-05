b10e241584e520965f2c74785205f3f7ff7ca8c7
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-04-05 23:42:25 +0530

    update

--- Queue/QueueUsingLL.cpp
+++ Queue/QueueUsingLL.cpp
@@ -12,68 +12,69 @@ public:
         next = NULL;
     }
 };
-class Queue
-{
-    Node *head;
-    Node *tail;
+// class Queue
+// {
+//     Node *head;
+//     Node *tail;
 
-public:
-    Queue()
-    {
-        head = tail = NULL;
-    }
-    void enQueue(int data)
-    {
-        Node *newNode = new Node(data);
+// public:
+//     Queue()
+//     {
+//         head = tail = NULL;
+//     }
+//     void enQueue(int data)
+//     {
+//         Node *newNode = new Node(data);
 
-        if (empty())
-        {
-            head = tail = newNode;
-        }
-        else
-        {
-            tail->next = newNode;
-            tail = newNode;
-        }
-    }
-    void dequeue()
-    {
-        if (empty())
-        {
-            cout << "LL is empty";
-            return;
-        }
+//         if (empty())
+//         {
+//             head = tail = newNode;
+//         }
+//         else
+//         {
+//             tail->next = newNode;
+//             tail = newNode;
+//         }
+//     }
+//     void dequeue()
+//     {
+//         if (empty())
+//         {
+//             cout << "LL is empty";
+//             return;
+//         }
 
-        Node *temp = head;
-        head = head->next;
-        delete temp;
-    }
-    int front()
-    {
-        if (empty())
-        {
-            cout << "LL is empty";
-            return -1;
-        }
-        return head->data;
-    }
-    bool empty()
-    {
-        return head == NULL;
-    }
-};
-int main()
-{
-    Queue q;
-    q.enQueue(10);
-    q.enQueue(20);
-    q.enQueue(30);
-    q.enQueue(40);
-    while (!q.empty())
-    {
-        cout << q.front() << " ";
-        q.dequeue();
-    }
-    cout << endl;
-    return 0;
-}
+//         Node *temp = head;
+//         head = head->next;
+//         delete temp;
+//     }
+//     int front()
+//     {
+//         if (empty())
+//         {
+//             cout << "LL is empty";
+//             return -1;
+//         }
+//         return head->data;
+//     }
+//     bool empty()
+//     {
+//         return head == NULL;
+//     }
+// };
+// int main()
+// {
+//     Queue q;
+//     q.enQueue(10);
+//     q.enQueue(20);
+//     q.enQueue(30);
+//     q.enQueue(40);
+//     while (!q.empty())
+//     {
+//         cout << q.front() << " ";
+//         q.dequeue();
+//     }
+//     cout << endl;
+//     return 0;
+// }
+
