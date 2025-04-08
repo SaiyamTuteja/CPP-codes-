9ec5250496107899672883ae22f67ec368217657
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-04-08 23:48:11 +0530

    update

--- Graph/graph.cpp
+++ Graph/graph.cpp
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct Node
+{
+    int data;
+    Node *left = NULL;
+    Node *right = NULL;
+};
+void preOrder(struct Node *rt, int value)
+{
+}
 int main()
 {
     return 0;
-    
 }
