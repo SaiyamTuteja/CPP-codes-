7aac8af9a5228b1cfc2aed2cb07f7015d5b02af3
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-04-29 18:28:41 +0530

    update

--- Linked List/PalandromeLL.cpp
+++ Linked List/PalandromeLL.cpp
@@ -51,7 +51,69 @@ public:
         return;
     }
 
-    Node *craeteNode(int data)
+    Node *reverse(Node *head)
+    {
+        Node *curr = head;
+        Node *prev = nullptr;
+        Node *next = nullptr;
+        while (curr != NULL)
+        {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    void palandrome1(Node *head)
+    {
+        if (!head || !head->next)
+        {
+            cout << "Yes, it's a palindrome Linked List\n";
+            return;
+        }
+
+        Node *slow = head, *fast = head;
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse second half
+        Node *secondHalf = reverse(slow);
+        Node *firstHalf = head;
+        Node *copySecondHalf = secondHalf; // To restore later
+
+        // Compare both halves
+        bool isPalindrome = true;
+        while (secondHalf != NULL)
+        {
+            if (firstHalf->data != secondHalf->data)
+            {
+                isPalindrome = false;
+                break;
+            }
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
+        }
+
+        // Restore the second half
+        reverse(copySecondHalf);
+
+        if (isPalindrome)
+        {
+            cout << "Yes, it's a palindrome Linked List\n";
+        }
+        else
+        {
+            cout << "No, it's not a palindrome Linked List\n";
+        }
+    }
+
+    Node *createNode(int data)
     {
         Node *newNode = new Node(data);
         newNode->next = NULL;
@@ -60,7 +122,7 @@ public:
     }
     Node *insertAtBegining(Node *head, int data)
     {
-        Node *newNode = craeteNode(data);
+        Node *newNode = createNode(data);
         newNode->next = head;
         head = newNode;
         return head;
@@ -86,9 +148,11 @@ int main()
     head = l.insertAtBegining(head, 30);
     head = l.insertAtBegining(head, 30);
     head = l.insertAtBegining(head, 20);
-    head = l.insertAtBegining(head, 60);
+    head = l.insertAtBegining(head, 10);
     l.print(head);
-    l.palandrome(head);
-
+    // l.palandrome(head);
+    // head = l.reverse(head);
+    l.palandrome1(head);
+    l.print(head);
     return 0;
 }
