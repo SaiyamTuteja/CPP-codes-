c24e95be47e3c9823ae2daa82fa2d32483eb0dff
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-05-05 18:30:51 +0530

    update

--- Strings/ReverseString.cpp
+++ Strings/ReverseString.cpp
@@ -1,5 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-int main(){
-    
+int main()
+{
+
+    string str = "this is Saiyam";
+    cout << "Before Reverse : " << str << endl;
+    // using two pointer approch
+    int start = 0;
+    int end = str.size() - 1;
+    while (start < end)
+    {
+        swap(str[start++], str[end--]);
+    }
+    cout << "After Reverse: " << str << endl;
+
+    // using cpp stl
+
+    reverse(str.begin(), str.end());
+    cout << str;
+
+    return 0;
 }
