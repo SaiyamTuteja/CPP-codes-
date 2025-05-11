45b8502b4a762f253214f07b2cc4543f071a18fc
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-05-11 14:42:27 +0530

    update

--- Hello.cpp
+++ Hello.cpp
@@ -1,91 +1,101 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define size 20
-int arr[size];
+
+#define SIZE 20
+int arr[SIZE];
 int top = -1;
+
 void push(int data)
 {
-    if (top == size - 1)
+    if (top == SIZE - 1)
     {
-        cout << "stack overflow";
+        cout << "Stack overflow\n";
     }
     else
     {
-        arr[top++] = data;
-        cout << "Element inserted succesfully " << data;
+        arr[++top] = data;
+        cout << "Element inserted successfully: " << data << endl;
     }
 }
+
 bool empty()
 {
     if (top == -1)
     {
-        cout << " stack is empty";
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-void pop()
-{
-    if (!empty())
-    {
-        return;
-    }
-    int temp = arr[top];
-    arr[top--];
-    cout << "Element deleted succesfully " << temp << endl;
-}
-bool isFull()
-{
-    if (top == size - 1)
-    {
-        cout << "element is full ";
+        cout << "Stack is empty\n";
         return true;
     }
     return false;
 }
+
+void pop()
+{
+    if (empty())
+    {
+        return;
+    }
+    int temp = arr[top--];
+    cout << "Element deleted successfully: " << temp << endl;
+}
+
+bool isFull()
+{
+    if (top == SIZE - 1)
+    {
+        cout << "Stack is full\n";
+        return true;
+    }
+    return false;
+}
+
 void print()
 {
-
-    for (int i = 0; i <= arr[top] - 1; i++)
+    if (empty())
+        return;
+    cout << "Stack elements: ";
+    for (int i = 0; i <= top; i++)
     {
         cout << arr[i] << " ";
     }
     cout << endl;
 }
+
 int main()
 {
-    int choice;
-    cout << "Enter the choice :";
-    cout << "1. push \n 2. pop \n 3. dispalay \n 4.empty \n 5. full \n 6. exit  \n ";
-    cin >> choice;
-    switch (choice)
+    while (true)
     {
-    case 1:
-        int data;
-        cout << "enter the element to push to the stack :";
-        cin >> data;
-        push(data);
-        break;
+        int choice;
+        cout << "\n1. Push\n2. Pop\n3. Display\n4. Check Empty\n5. Check Full\n6. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-    case 2:
-        pop();
-        break;
-    case 4:
-        print();
-        break;
-    case 5:
-        empty();
-        break;
-    case 6:
-        isFull();
-        break;
-    default:
-        cout << "enter the valid choice " << endl;
-        break;
+        switch (choice)
+        {
+        case 1:
+        {
+            int data;
+            cout << "Enter the element to push to the stack: ";
+            cin >> data;
+            push(data);
+            break;
+        }
+        case 2:
+            pop();
+            break;
+        case 3:
+            print();
+            break;
+        case 4:
+            empty();
+            break;
+        case 5:
+            isFull();
+            break;
+        case 6:
+            cout << "Exiting program.\n";
+            return 0;
+        default:
+            cout << "Enter a valid choice.\n";
+        }
     }
-    return 0;
 }
