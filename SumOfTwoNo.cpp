f5040c48740a2a4f1558e87068b0cec94b4e0230
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-05-31 23:12:28 +0530

    upadate

--- SumOfTwoNo.cpp
+++ SumOfTwoNo.cpp
@@ -1,15 +1,3 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
-int main()
-{
-    int n;
-    int m;
-    cout << "Enter the value of n: ";
-    cin >> n;
-    cout << "Enter the value of m: ";
-    cin >> m;
-    cout << "Sum of " << n << " and " << m << " is " << n + m;
-    return 0;
-}
