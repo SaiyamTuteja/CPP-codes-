127ece3292c5151afeeeb6e17770b4deff60caed
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-06-06 00:47:35 +0530

    upadate

---
+++ Graph/CreationOfGraph.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define size 10
+void PrintAdjacencyMatrix(int adj[][size]){
+    for( int i =0;i<size;i++){
+        for(int j=0;j<size;i++){
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+
+}
