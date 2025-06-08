ab7e1f69599ad71ec2372eab00b572e2d885fc36
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-06-08 23:47:31 +0530

    upadate

--- Graph/CreationOfGraph.cpp
+++ Graph/CreationOfGraph.cpp
@@ -1,25 +1,29 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-#define size 10
-void PrintAdjacencyMatrix(int adj[][size])
+
+void PrintAdjacencyMatrix(vector<vector<int>> &adjMatrix, int n)
 {
-    for (int i = 0; i < size; i++)
+    cout << "\nAdjacency Matrix:" << endl;
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < size; i++)
+        for (int j = 0; j < n; j++)
         {
-            cout << adj[i][j] << " ";
+            cout << adjMatrix[i][j] << " ";
         }
         cout << endl;
     }
 }
-void PrintAdjacancyList(vector<int> adj[size + 1])
+
+void PrintAdjacencyList(vector<vector<int>> &adjList, int n)
 {
-    for (int i = 0; i < size; i++)
+    cout << "\nAdjacency List:" << endl;
+    for (int i = 0; i < n; i++)
     {
-        cout << i << " ";
-        for (auto x : adj[i])
+        cout << i << ": ";
+        for (int v : adjList[i])
         {
-            cout << x << " ";
+            cout << v << " ";
         }
         cout << endl;
     }
@@ -28,14 +32,43 @@ void PrintAdjacancyList(vector<int> adj[size + 1])
 int main()
 {
     int n, m;
-    cout << "Emter the edges : ";
+    cout << "Enter the number of vertices: ";
     cin >> n;
-    cout << "Enter the vertexs : ";
+    cout << "Enter the number of edges: ";
     cin >> m;
-    int adj[n][m];
+
+    // Create dynamic adjacency matrix and list
+    vector<vector<int>> adjMatrix(n, vector<int>(n, 0));
+    vector<vector<int>> adjList(n);
+
+    cout << "Enter " << m << " edges (as pairs of vertices u v):" << endl;
     for (int i = 0; i < m; i++)
     {
         int u, v;
-        cin >> u >> V;
+        cin >> u >> v;
+
+        // Add edge to matrix
+        adjMatrix[u][v] = 1;
+        adjMatrix[v][u] = 1; // remove for directed graph
+
+        // Add edge to list
+        adjList[u].push_back(v);
+        adjList[v].push_back(u); // remove for directed graph
     }
+
+    // Print both representations
+    PrintAdjacencyMatrix(adjMatrix, n);
+    PrintAdjacencyList(adjList, n);
+
+    return 0;
 }
+
+/*
+Sample Input
+        Enter the number of vertices: 4
+        Enter the number of edges: 3
+        Enter 3 edges (as pairs of vertices u v):
+        0 1
+        1 2
+        2 3
+*/
