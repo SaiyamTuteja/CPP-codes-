5fad07179be658d0a285cbebc6e01cb2c497d7c9
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-06-19 23:56:19 +0530

    upadate

--- Graph/BFS_traversal.cpp
+++ Graph/BFS_traversal.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define size 10
-int vistied[10]={0};
+int vistied[10] = {0};
+int graph[10][10] = {};
 void BFS_Traversal(vector<int> visited, int N)
 {
-
 }
 int main()
 {
