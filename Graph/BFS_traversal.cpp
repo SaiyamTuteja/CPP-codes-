36c73987934d87b725ea7c8e54772999b164fea0
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-06-19 23:57:27 +0530

    upadate

--- Graph/BFS_traversal.cpp
+++ Graph/BFS_traversal.cpp
@@ -3,8 +3,10 @@ using namespace std;
 #define size 10
 int vistied[10] = {0};
 int graph[10][10] = {};
+queue<int> q1;
 void BFS_Traversal(vector<int> visited, int N)
 {
+    q1.push(start);
 }
 int main()
 {
