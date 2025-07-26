b9ac09086dc7f7f6e26afbbbad0369b7959d8eb0
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-07-26 10:54:37 +0530

    update

--- Linked List/try.cc
+++ Linked List/try.cc
@@ -1,72 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct node {
+struct node
+{
     int data;
-    struct node* next;
+    struct node *next;
 };
 
-struct node* createNode(int data) {
-    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+struct node *createNode(int data)
+{
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
     newNode->data = data;
     newNode->next = nullptr;
     return newNode;
 }
 
-void PushStart(int data, struct node*& head) {
-    struct node* newNode = createNode(data);
+void PushStart(int data, struct node *&head)
+{
+    struct node *newNode = createNode(data);
     newNode->next = head;
     head = newNode;
 }
 
-void PushEnd(int data, struct node*& head) {
-    struct node* newNode = createNode(data);
-    if (head == nullptr) {
+void PushEnd(int data, struct node *&head)
+{
+    struct node *newNode = createNode(data);
+    if (head == nullptr)
+    {
         head = newNode;
         return;
     }
-    struct node* temp = head;
-    while (temp->next != nullptr) {
+    struct node *temp = head;
+    while (temp->next != nullptr)
+    {
         temp = temp->next;
     }
     temp->next = newNode;
 }
 
-void PushATPos(int data, struct node*& head, int pos) {
-    if (pos == 1) {
+void PushATPos(int data, struct node *&head, int pos)
+{
+    if (pos == 1)
+    {
         PushStart(data, head);
         return;
     }
 
-    struct node* temp = head;
+    struct node *temp = head;
     int currPos = 1;
 
-    while (temp != NULL && currPos < pos - 1) {
+    while (temp != NULL && currPos < pos - 1)
+    {
         temp = temp->next;
         currPos++;
     }
 
-    if (temp == nullptr) {
+    if (temp == nullptr)
+    {
         cout << "Position out of bounds\n";
         return;
     }
 
-    struct node* newNode = createNode(data);
+    struct node *newNode = createNode(data);
     newNode->next = temp->next;
     temp->next = newNode;
 }
 
-void Display(struct node* head) {
-    struct node* temp = head;
-    while (temp != nullptr) {
+void Display(struct node *head)
+{
+    struct node *temp = head;
+    while (temp != nullptr)
+    {
         cout << temp->data << " -> ";
         temp = temp->next;
     }
     cout << "NULL" << endl;
 }
 
-int main() {
-    struct node* head = NULL;
+int main()
+{
+    struct node *head = NULL;
 
     PushStart(10, head);
     Display(head);
