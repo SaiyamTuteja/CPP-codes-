13d138e5f866a52ffa27f7345994cd5a5ecc43d9
Saiyam Tuteja <saiyamtuteja@gmail.com>
2025-08-29 01:07:14 +0530

    update tree

---
+++ Tree/treeDemo.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+
+    static void inorder(Node *root)
+    {
+        if (root == nullptr)
+            return;
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+    static void preorder(Node *root)
+    {
+        if (root == nullptr)
+            return;
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+    static void postorder(Node *root)
+    {
+        if (root == nullptr)
+            return;
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
+    }
+    static int CountNodes(Node *root)
+    {
+
+        if (root == NULL)
+            return 0;
+        int leftCount = CountNodes(root->left);
+        int rightCount = CountNodes(root->right);
+        return leftCount + rightCount + 1;
+    }
+    static Node *insertNode(Node *root, int data)
+    {
+        if (root == nullptr)
+        {
+            return new Node(data);
+        }
+        if (data > root->data)
+        {
+            root->right = insertNode(root->right, data);
+        }
+        else
+        {
+            root->left = insertNode(root->left, data);
+        }
+        return root;
+    }
+};
+
+int main()
+{
+    // int data;
+    // Node obj(data);
+    Node *root = nullptr;
+    int values[] = {10, 20, 30, 40, 50, 60, 90, 30, 50, 60};
+    for (int val : values)
+    {
+        root = Node::insertNode(root, val);
+    }
+    cout << "Total nodes: " << Node::CountNodes(root) << endl;
+}
